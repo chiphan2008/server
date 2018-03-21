d80bae057abed64aca53ba54fb988d7123d8610f
Chi Phan <chiphan2008@gmail.com>
2018-03-21 22:27:14 +0700

    shjdf

---
+++ app/models/person.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PersonShema = new Schema({
+  id:Number,
+  name:String,
+  urlhinh:String,
+});
+
+module.exports = mongoose.model('Person',PersonShema)
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var server = require('http').Server(app);
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var router = express.Router();
+var Person = require('./app/models/person')
+
 mongoose.connect('mongodb://localhost:27017/chat');
 //var io = require('socket.io')(server);
 server.listen(2309,'112.213.94.96');
@@ -12,14 +14,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.bodyParser());
 app.use('/api',router);
+
+app.use(function(req, res, next){
+  next();
+})
+
 router.get('/',function(req,res){
 	res.json({message:"Welcome to RESTFUL API NodeJS"})
 })
 
-router.post('/', function(req, res) {
-    let data = {
-        response: 'You sent: ' + req.body.message
-    };
+router.route('/person')
+        .post(function(req, res){
+          var person = new Person();
+          person.id = req.body.id;
+          person.name = req.body.name;
+          person.urlhinh = req.body.urlhinh;
 
-    res.status(200).send(data);
-});
+          person.save(function(err){
+            if(err){
+              res.json({error:err})
+            }
+            res.json({message:'Data inserted successful!'})
+          })
+        })
+        .get(function(req, res){
+          Person.find(function(err, person){
+            if(err) res.json({error:err})
+            res.json({person})
+          });
+        })
+// router.post('/', function(req, res) {
+//     let data = {
+//         response: 'You sent: ' + req.body.message
+//     };
+//
+//     res.status(200).send(data);
+// });
