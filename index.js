863ed0ccce11f6d19f051948ba9777a78aa914e3
Chi Phan <chiphan2008@gmail.com>
2018-03-21 21:38:55 +0700

    sdsd

--- index.js
+++ index.js
@@ -11,20 +11,14 @@ server.listen(2309,'112.213.94.96');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.bodyParser());
-<<<<<<< HEAD
-
-app.get('/',function(req,res){
-	res.send(JSON.stringify({message:"Welcome to RESTFUL API NodeJS"}))
-=======
 app.use('/api',router);
 router.get('/',function(req,res){
 	res.json({message:"Welcome to RESTFUL API NodeJS"})
->>>>>>> 6e4e744479933a35d3161a85ff1407b0ea97d048
 })
 
 router.post('/', function(req, res) {
     let data = {
-        response: 'You sent: ' + req.body
+        response: 'You sent: ' + req.body.message
     };
 
     res.status(200).send(data);
