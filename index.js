ec027f69f49c5db96585116079b13f1c6cb73aa0
Chi Phan <chiphan2008@gmail.com>
2018-03-21 15:55:05 +0700

    test thu

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+const bodyParser = require('body-parser');
 //var io = require('socket.io')(server);
 server.listen(2309,'112.213.94.96');
-console.log('Server running at http://112.213.94.96:2309');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/',function(req,res){
-	res.end(JSON.stringify({message:"Welcome to NodeJS"}))
+	res.send(JSON.stringify({message:"Welcome to RESTFUL API NodeJS"}))
 })
 
+app.post('/', function(req, res) {
+    let data = {
+        response: 'You sent: ' + req.body.message
+    };
+
+    res.status(200).send(data);
+});
