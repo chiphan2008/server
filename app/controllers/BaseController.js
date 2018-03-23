e3f13a05cf0c107891a89d30d39052b53a2c76a4
Chi Phan <chiphan2008@gmail.com>
2018-03-23 11:58:12 +0700

    ddsd

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -1,6 +1,6 @@
+var io = require('socket.io')(server, {pingTimeout: 30000});
 
 exports.createNSP = function(port){
-  var io = require('socket.io')(server, {pingTimeout: 30000});
   var nsp = io.of('/'+port);
   nsp.on('connection', function(socket) {
      nsp.on('sendMessage',function(data){
