7ce34b8a952e3ee20c909b558e24c5e9dc37b640
Chí Phan <chiphan2008@gmail.com>
2018-03-28 13:58:00 +0700

    dfdF

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -1,5 +1,5 @@
 
-exports.findOneMessage = (param) => {
+exports.findOneMessage = function(param){
   var Conversation = require('../models/Conversation')
   Conversation.find({group:param}).sort('-create_at').limit(1).exec(function(err, el){
     return el;
