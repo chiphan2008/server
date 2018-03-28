3b4227de126a3ad93bb861cd3ef7272d13261b28
Chí Phan <chiphan2008@gmail.com>
2018-03-28 22:11:02 +0700

    dfd

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -1,17 +1,20 @@
 'use strict';
-async = require('async');
+var async = require('async');
 var Conversation = require('../models/Conversation')
 
-let findOneMessage = (param) => {
-  return new Promise(function(resolve, reject) {
-    try{
-      Conversation.findOne({group:param}).sort('-create_at').exec((err,el)=>{
-        resolve(el)
-      })
-    }catch(err){
-      reject(err)
-    }
-  })
+let findOneMessage = async (param) => {
+  let Conv = () => {
+    return new Promise(function(resolve, reject) {
+      try{
+        Conversation.findOne({group:param}).sort('-create_at').exec((err,el)=>{
+          resolve(el)
+        })
+      }catch(err){
+        reject(err)
+      }
+    })
+  }
+  return await Conv();
 }
 
 module.exports.findOneMessage = findOneMessage;
