a183b3fe0a956cf640df71245522c0cb6a18031c
Chí Phan <chiphan2008@gmail.com>
2018-03-28 15:13:17 +0700

    dfD

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -3,14 +3,7 @@
 
 var Conversation = require('../models/Conversation')
 
-module.exports = {
 
-  // async getPosts (perPage = 10) {
-  //   const {data} = await axios.get(endpoint + '/posts?_embed&per_page=' + perPage)
-  //   return data
-  // }
-   function  findOneMessage(param){
-    return  Conversation.findOne({group:param}).sort('-create_at').exec();
-  }
-
+exports.findOneMessage =  (param) =>{
+  return  Conversation.findOne({group:param}).sort('-create_at').exec();
 }
