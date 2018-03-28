ffc23f7f614c4eec4577ae25da2066db54577bb4
Chí Phan <chiphan2008@gmail.com>
2018-03-28 15:22:25 +0700

    dfD

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -4,6 +4,6 @@
 var Conversation = require('../models/Conversation')
 
 
-exports.findOneMessage =  (param) =>{
-  return  Conversation.findOne({group:param}).sort('-create_at').exec();
+exports.findOneMessage = (param) =>{
+  return  Conversation.findOne({group:param}).sort('-create_at').execSync();
 }
