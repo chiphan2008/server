b645017af1589e7b8f455560a4cf867812f5d774
Chí Phan <chiphan2008@gmail.com>
2018-03-28 16:53:11 +0700

    dfD

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -4,14 +4,13 @@
 var Conversation = require('../models/Conversation')
 
 exports.findOneMessage = (param) =>{
-  var p1 = new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
       if(params!==''){
         Conversation.findOne({group:param}).sort('-create_at').exec((err,el)=>{
           resolve(el)
         })
       }
-  })
-  p1.then(el=>{
+  }).then(el=>{
     return el;
   })
   //return Conversation.findOne({group:param}).sort('-create_at').exec()
