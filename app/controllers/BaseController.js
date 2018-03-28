f9aea7cd577da304a05cf33a78ac0ef00231e83c
Chí Phan <chiphan2008@gmail.com>
2018-03-28 22:02:32 +0700

    dfd

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -4,13 +4,16 @@
 var Conversation = require('../models/Conversation')
 
 let findOneMessage = async (param) => {
+
   let Conv = () =>{
     return new Promise(function(resolve, reject) {
-        if(param!==''){
-          Conversation.findOne({group:param}).sort('-create_at').exec((err,el)=>{
-            resolve(el)
-          })
-        }
+      try{
+        Conversation.findOne({group:param}).sort('-create_at').exec((err,el)=>{
+          resolve(el)
+        })
+      }catch(err){
+        reject(err)
+      }
     })
   }
   return await Conv();
