545307007a01f480845b80bc046bda00040c99bf
Chí Phan <chiphan2008@gmail.com>
2018-03-28 14:02:42 +0700

    dfdF

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -1,6 +1,7 @@
+'use strict';
+var Conversation = require('../models/Conversation')
 
 exports.findOneMessage = function(param){
-  //var Conversation = require('../models/Conversation')
   Conversation.find({group:param}).sort('-create_at').limit(1).exec(function(err, el){
     return el;
   });
