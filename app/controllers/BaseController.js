e63b26d3e91d5b920212d0fdcb2b617be7a519b6
Chí Phan <chiphan2008@gmail.com>
2018-03-28 14:32:31 +0700

    dfD

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -3,7 +3,7 @@ async = require('async');
 
 var Conversation = require('../models/Conversation')
 
-exports.findOneMessage = async(param) =>{
+exports.findOneMessage = (param) =>{
   Conversation.findOne({group:param}).sort('-create_at').exec(function(err, el){
     return el;
   });
