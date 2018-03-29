be0dcc4d150d853f7f8df8a5dc1297353fcba1b6
Chí Phan <chiphan2008@gmail.com>
2018-03-29 15:58:39 +0700

    dfdg

--- app/controllers/BaseController.js
+++ app/controllers/BaseController.js
@@ -4,8 +4,8 @@ var Conversation = require('../models/Conversation')
 // exports.findOneMessage = (param) => {
 //   return Conversation.findOne({group:param}).sort('-create_at').exec()
 // }
-exports.findListFriend = (user_id) => {
-  return new Promise((resolve,reject)=>{
+findListFriend.exports = (user_id) => {
+  return new Promise(function(resolve,reject){
     Person.findOne({id:user_id}).exec(function(err, el){
       if(err) reject(err)
       resolve(el)
