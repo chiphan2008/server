02f3e6628c6da40107f07bf5568749148a3c18ad
Chí Phan <chiphan2008@gmail.com>
2018-03-29 08:09:19 +0700

    dfdg

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ router.route('/person')
 router.route('/except-person/:id')
         .get(function(req, res){
           if(req.params.id>0){
-            Person.find({id:{$ne : req.params.id}}).sort('_id').exec(function(err, data){
+            Person.find({id:{$ne : req.params.id}}).sort('-_id').exec(function(err, data){
               res.json({data});
             });
           }else {
