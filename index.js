3ad1fb8a1183e9ff1da5b1f726d39381a218214d
Chí Phan <chiphan2008@gmail.com>
2018-03-30 15:50:41 +0700

    dfd

--- index.js
+++ index.js
@@ -133,13 +133,15 @@ router.route('/list-friend/:id/:status')
           if(req.params.id>0){
             ListFriend.findOne({id:req.params.id}).exec(function(err, arr){
               if(arr!==null){
+                var item=[];
                 var p = new Promise((resolve,reject)=>{
-                  var item=[];
                   arr.friends.forEach((e,i)=>{
                     //res.json({e:e.status,red:req.params.status})
                     if(e.status===req.params.status){
                       item.push(e);
                       resolve(item);
+                    }else{
+                      reject(item);
                     }
 
                   })
