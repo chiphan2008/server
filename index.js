13bd6a6de2ef1b83b96741b3e3c993610b61cfc8
Chí Phan <chiphan2008@gmail.com>
2018-03-30 15:29:20 +0700

    dfd

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ router.route('/list-friend/:id/:status')
               if(arr!==null){
                 var data=[];
                 arr.friends.forEach((e,i)=>{
-                  res.json({params:req.params.status,e:e.status})
+                  //res.json({params:req.params.status,e:e.status})
                   if(e.status===req.params.status) data.push(e);
                   if(i===arr.friends.length-1) res.json({data})
                 })
