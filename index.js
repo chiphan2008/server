193bbee3098edd66120741f42d45aec9fcdf566d
Chí Phan <chiphan2008@gmail.com>
2018-03-30 08:17:20 +0700

    dfd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 var Person = require('./app/models/person')
 var Conversation = require('./app/models/Conversation')
 //var ListFriend = require('./app/models/ListFriend')
-var BaseController = require('./app/controllers/BaseController')
+//var BaseController = require('./app/controllers/BaseController')
 mongoose.connect('mongodb://localhost:27017/chat');
 server.listen(2309,'112.213.94.96');
 
