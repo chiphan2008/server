79965f281bc733d2a5c93ba3cbcc6fec245efee2
Jimmy <chiphan2008@gmail.com>
2018-06-13 16:25:57 +0700

    fsdffsd'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var Person = require('./app/models/person')
 var Conversation = require('./app/models/Conversation')
 var ListFriend = require('./app/models/ListFriend')
 //var BaseController = require('./app/controllers/BaseController')
-const hostname = '112.213.94.96';
+const hostname = 'node.kingmap.vn';
 const port = 2309;
 mongoose.connect('mongodb://localhost:27017/chat');
 server.listen(port, hostname, () => {
