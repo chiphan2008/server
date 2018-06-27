c4bb515a2ea4e89cf8394831320ee6324045f1f8
Jimmy <chiphan2008@gmail.com>
2018-06-27 14:43:45 +0700

    dfgdfg

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ router.route('/person/:id')
         .get(function(req, res){
           if(req.params.id>0){
             Person.find({id:req.params.id}).exec(function(err, data){
-              res.json({data});
+              res.json({data:data[0]});
             });
           }else {
             res.json({error:"Cant not GET"})
