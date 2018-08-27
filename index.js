2e119e496a329f44df62c11f7ed052abfb7cd983
Jimmy <chiphan2008@gmail.com>
2018-08-27 17:15:19 +0700

    Fg

--- index.js
+++ index.js
@@ -233,14 +233,14 @@ router.route('/list-friend/:id').get(function(req, res){
 router.route('/list-friend/:id/:status').get(function(req, res){
           if(req.params.id>0){
             Person.aggregate(
-              {$match: {id:req.params.id}},
-              { $project: {
-                  friends: {$filter: {
-                      input: '$friends',
-                      as: 'friend',
-                      cond: {$eq: ['$$friend.addfriend', req.params.status]}
-                  }}}
-              }
+              {$match: {id:req.params.id}}
+              // { $project: {
+              //     friends: {$filter: {
+              //         input: '$friends',
+              //         as: 'friend',
+              //         cond: {$eq: ['$$friend.addfriend', req.params.status]}
+              //     }}}
+              // }
             	// {$addFields : {"friends":{$filter:{ // We override the existing field!
             	// 	input: "$friends",
             	// 	as: "friend",
