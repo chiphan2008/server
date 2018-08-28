0270118a84457a004b7ac54747bd52429e01a95c
Jimmy <chiphan2008@gmail.com>
2018-08-28 09:10:02 +0700

    Fg

--- index.js
+++ index.js
@@ -229,10 +229,9 @@ router.route('/list-friend/:id').get(function(req, res){
 })
 router.route('/list-friend/:id/:status').get(function(req, res){
           if(req.params.id>0){
-            ListFriend.aggregate(
-              [{$match: {id:req.params.id}},
+            ListFriend.aggregate([
+              {$match: {id:req.params.id}},
               { $project: {
-                  id:req.params.id,
                   friends: {
                     $filter: {
                       input: "$friends",
