76fe3d611d7d8380fcd50def36a0e7faae4ffe95
Jimmy <chiphan2008@gmail.com>
2018-08-28 09:32:26 +0700

    Fg

--- index.js
+++ index.js
@@ -228,10 +228,11 @@ router.route('/list-friend/:id').get(function(req, res){
           }
 })
 router.route('/list-friend/:id/:status').get(function(req, res){
+          res.json({id:req.params.id,param:req.params.status})
           if(req.params.id>0){
             ListFriend.aggregate([
+              { $match : { id : req.params.id } },
               { $project: {
-                  id:req.params.id,
                   friends: {
                     $filter: {
                       input: "$friends",
