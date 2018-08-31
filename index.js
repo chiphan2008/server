562f82d58cafb8141769196a3875a2ac0ab66b8a
Jimmy <chiphan2008@gmail.com>
2018-08-31 08:16:26 +0700

    Fgfgfg

--- index.js
+++ index.js
@@ -273,9 +273,9 @@ router.route('/list-friend/:id/:status').get(function(req, res){
                 "from":"listfriends",
                 "let":{"id":"$id"},
                 "pipeline":[
-                  {"$match":{"$expr":{"$in":["$$id","$friends"]}}},
+                  {"$match":{"$expr":{"$in":["$$id","$friends.friend_id"]}}},
                   {"$unwind":"$friends"},
-                  {"$match":{"$expr":{"$eq":["$$id","$friends"]}}}
+                  {"$match":{"$expr":{"$eq":["$$id","$friends.friend_id"]}}}
                 ],
                 "as":"list_friends"
               }}
