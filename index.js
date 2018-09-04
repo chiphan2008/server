844fae49f0350d3f87f598337b059d806569aa71
Jimmy <chiphan2008@gmail.com>
2018-09-04 16:55:45 +0700

    fgHfhfgh

--- index.js
+++ index.js
@@ -215,17 +215,23 @@ router.route('/static-friend/:id').get(function(req, res){
                   accept:{ $reduce: {
                       input: "$accept.count",
                       initialValue: 1,
-                      in: { $multiply: [ "$$value", "$$this" ] }
+                      in: { $multiply: [ "$$value",
+                      {'$cond': [{'$eq': ['$$value', null]}, 0, 1]},
+                      "$$this" ] }
                   }},
                   waiting:{ $reduce: {
                       input: "$waiting.count",
                       initialValue: 1,
-                      in: { $multiply: [ "$$value", "$$this" ] }
+                      in: { $multiply: [ "$$value",
+                      {'$cond': [{'$eq': ['$$value', null]}, 0, 1]},
+                      "$$this" ] }
                   }},
                   request:{ $reduce: {
                       input: "$request.count",
                       initialValue: 1,
-                      in: { $multiply: [ "$$value", "$$this" ] }
+                      in: { $multiply: [ "$$value",
+                      {'$cond': [{'$eq': ['$$value', null]}, 0, 1]},
+                      "$$this" ] }
                   }}
               }}
             ]).exec(function(err, arr){
