212609db3d2b38a6fd25475fc7ea81e20981d4b8
Jimmy <chiphan2008@gmail.com>
2018-09-04 12:02:27 +0700

    fgHfhfgh

--- index.js
+++ index.js
@@ -201,8 +201,10 @@ router.route('/static-friend/:id').get(function(req, res){
             ListFriend.aggregate([
               {"$match":{"id":parseInt(req.params.id)}},
               {$unwind: "$friends" },
-              {$group: { _status: "$friends.status", count: { $sum: 1 } }},
-              {$project: {status:"$_status",count:1}}
+              {$group: { _id: "$friends.status", count: { $sum: 1 } }},
+              {$project:
+                {status:"$_id",count:1}
+              }
             ]).exec(function(err, arr){
                   if(arr===null || err){
                       if(err) res.json(err)
