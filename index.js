406be830a1d101e3af18ed87d8f86c5a39b4c8b9
Jimmy <chiphan2008@gmail.com>
2018-09-04 15:21:34 +0700

    fgHfhfgh

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ router.route('/static-friend/:id').get(function(req, res){
               {$group:{_id:0,newData:{$push:
                 {"k":"$status","v":"$count"}
               }}},
-              {$unwind: "$data" },
+              {$unwind: "$newData" },
               {$project:{_id:0,data:{$arrayToObject:  { $literal: "$newData"}} }}
             ]).exec(function(err, arr){
                   if(arr===null || err){
