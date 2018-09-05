2422a2aebb394366f1f33f1571c66da564ea365b
Jimmy <chiphan2008@gmail.com>
2018-09-05 17:07:56 +0700

    fgHfhfgh

--- index.js
+++ index.js
@@ -100,11 +100,10 @@ router.route('/person/offline').post(function(req, res){
            });
 })
 router.route('/person/update').post(function(req, res){
-          //console.log('/person/update',req.body);
           //res.json({request:req.body});
       if(parseInt(req.body.id)>0){
         const dateNow =Date.now();
-        const obj = {
+        let obj = {
           "name": req.body.name,
           "urlhinh": req.body.urlhinh,
           "email": req.body.email,
