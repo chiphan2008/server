95accd4083f43bdd2d38d13b7b6ef95c9d0aeae1
Jimmy <chiphan2008@gmail.com>
2018-09-12 14:52:06 +0700

    fgf

--- index.js
+++ index.js
@@ -185,10 +185,10 @@ router.route('/except-person/:id').get(function(req, res){
             });
           }else { res.json({error:"Can not GET"}) }
   })
-router.route('/search-person/:kw').get(function(req, res){
+router.route('/search-person').post(function(req, res){
           var skipping = parseInt(req.query.skip) || 0;
           var limiting = parseInt(req.query.limit) || 0;
-          const kw = req.params.kw;
+          const kw = req.body.keyword;
           Person.find({ $or :[{name:/kw/},{email:/kw/},{phone:/kw/}]})
           .limit(limiting).skip(skipping)
           .exec(function(err, data){
